8beb832390f6d1ee8f25db41189258839659377d
poyu <2381150086@qq.com>
2019-06-07 18:30:16 +0800

    车辆违章处理

--- src/test/ImageDemo.java
+++ src/test/ImageDemo.java
@@ -8,12 +8,12 @@ import java.io.IOException;
 public class ImageDemo {
 
     public void binaryImage() throws IOException{
-	File file = new File("res/image/test_image/1/1.PNG");
+	File file = new File("res/image/test_image/1/2.PNG");
 	BufferedImage image = ImageIO.read(file);
-	
+
 	int width = image.getWidth();
 	int height = image.getHeight();
-	
+
 	BufferedImage grayImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_BINARY);//重点，技巧在这个参数BufferedImage.TYPE_BYTE_BINARY
 	for(int i= 0 ; i < width ; i++){
 	    for(int j = 0 ; j < height; j++){
@@ -21,18 +21,18 @@ public class ImageDemo {
 		grayImage.setRGB(i, j, rgb);
 	    }
 	}
-	
-	File newFile = new File("res/image/test_image/1/1.PNG");
+
+	File newFile = new File("res/image/test_image/1/2.PNG");
 	ImageIO.write(grayImage, "jpg", newFile);
     }
-    
+
     public void grayImage() throws IOException{
-	File file = new File("res/image/test_image/1/1.PNG");
+	File file = new File("res/image/test_image/1/2.PNG");
 	BufferedImage image = ImageIO.read(file);
-	
+
 	int width = image.getWidth();
 	int height = image.getHeight();
-	
+
 	BufferedImage grayImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);//重点，技巧在这个参数BufferedImage.TYPE_BYTE_GRAY
 	for(int i= 0 ; i < width ; i++){
 	    for(int j = 0 ; j < height; j++){
@@ -40,15 +40,15 @@ public class ImageDemo {
 		grayImage.setRGB(i, j, rgb);
 	    }
 	}
-	
-	File newFile = new File("res/image/test_image/1/1.PNG");
+
+	File newFile = new File("res/image/test_image/1/2.PNG");
 	ImageIO.write(grayImage, "jpg", newFile);
     }
-    
+
     public static void main(String[] args) throws IOException {
 	ImageDemo demo = new ImageDemo();
 	demo.binaryImage();
 	demo.grayImage();
     }
- 
+
 }
